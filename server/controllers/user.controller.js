cc57d8d916ab86acc6c945c40367b5eaee9c71fe
abhay8696 <kambleabhaygautam@gmail.com>
2024-10-11 14:00:25 +0530

    created user controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,16 @@
+const httpStatus = require("http-status");
+const { userService } = require("../services");
+
+const createUser = catchAsync(async (req, res) => {
+    const create = await userService.createUser(req.body);
+
+    res.status(httpStatus.CREATED).send(create);
+});
+
+const getUserByEmail = catchAsync(async (req, res) => {
+    const user = await userService.getUserByEmail(req.body.contact);
+
+    res.status(httpStatus.OK).send(user);
+});
+
+module.exports = { createUser, getUserByEmail };
