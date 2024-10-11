98b7e61751e98ace69e54167433a0f219e893ea6
abhay8696 <kambleabhaygautam@gmail.com>
2024-10-11 14:05:08 +0530

    created routes for user creation and fetching

---
+++ server/routes/user.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { userController } = require("../controllers");
+
+const router = express.Router();
+
+router.post("/new", userController.createUser);
+
+router.get("/email", userController.getUserByEmail);
+
+module.exports = router;
